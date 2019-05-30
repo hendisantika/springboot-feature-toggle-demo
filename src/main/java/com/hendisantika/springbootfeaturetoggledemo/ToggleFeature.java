dac4a3dbe281064de2f8ba2f4e85a52638aafd45
hendisantika <hendisantika@yahoo.co.id>
2019-05-30 07:14:30 +0700

    #4 - Add ToggleFeature

---
+++ src/main/java/com/hendisantika/springbootfeaturetoggledemo/ToggleFeature.java
@@ -0,0 +1,39 @@
+package com.hendisantika.springbootfeaturetoggledemo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-feature-toggle-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-30
+ * Time: 07:14
+ */
+public class ToggleFeature {
+    private String name;
+    private boolean active;
+
+    public ToggleFeature() {
+    }
+
+    public ToggleFeature(String name, boolean active) {
+        this.name = name;
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
